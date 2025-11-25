2a89e5cc46dabc18189da22f876b16db9c6808cb
jesicamatul <evelinmtlj@gmail.com>
2025-11-25 10:48:25 -0500

    scripts

---
+++ scripts.sql
@@ -0,0 +1,46 @@
+ use dealership;
+
+-- CREATE TABLE sales_contracts (
+-- id INT auto_increment PRIMARY KEY,
+-- vin VARCHAR(17),
+-- customer_name VARCHAR(50),
+-- sale_price DECIMAL(10,2),
+-- sale_date DATE,
+-- FOREIGN KEY (vin) references vehicles(vin)
+-- );
+
+-- insert into dealerships ( name , address , phone)
+-- value ('Auto World', '123 Main St', '555-111-2222'),
+-- ('Speed Motors', '456 Oak Ave', '555-333-4444'),
+-- ('Prime Cars', '789 Pine Rd', '555-555-6666');
+
+
+-- Insert into vehicles ( vin , year, make, model, vehicle_type, color, odometer,price,sold)
+-- values ('1A2B3C4D5E6F7G8H1', 2020, 'Toyota', 'Camry', 'Sedan', 'Blue', 35000, 19000.00, FALSE),
+-- ('2B3C4D5E6F7G8H9I2', 2018, 'Honda', 'Civic', 'Sedan', 'Red', 42000, 15000.00, FALSE),
+-- ('3C4D5E6F7G8H9I1J3', 2022, 'Ford', 'F-150', 'Truck', 'Black', 12000, 38000.00, TRUE),
+-- ('4D5E6F7G8H9I1J2K4', 2019, 'Nissan', 'Rogue', 'SUV', 'White', 28000, 21000.00, FALSE),
+-- ('5E6F7G8H9I1J2K3L5', 2021, 'Chevrolet', 'Malibu', 'Sedan', 'Silver', 15000, 22000.00, FALSE),
+-- ('6F7G8H9I1J2K3L4M6', 2020, 'Hyundai', 'Tucson', 'SUV', 'Gray', 25000, 24000.00, TRUE),
+-- ('7G8H9I1J2K3L4M5N7', 2017, 'Kia', 'Optima', 'Sedan', 'Black', 50000, 13000.00, FALSE),
+-- ('8H9I1J2K3L4M5N6O8', 2023, 'Tesla', 'Model 3', 'Electric', 'White', 5000, 42000.00, FALSE);
+
+--  Insert into inventory ( dealership_id, vin)
+--  Values (1000, '1A2B3C4D5E6F7G8H1'),
+-- (1000, '2B3C4D5E6F7G8H9I2'),
+
+-- (1001, '3C4D5E6F7G8H9I1J3'),
+-- (1001, '4D5E6F7G8H9I1J2K4'),
+-- (1001, '5E6F7G8H9I1J2K3L5'),
+
+-- (1002, '6F7G8H9I1J2K3L4M6'),
+-- (1002, '7G8H9I1J2K3L4M5N7'),
+-- (1002, '8H9I1J2K3L4M5N6O8');
+
+select * from inventory
+
+
+
+
+
+
